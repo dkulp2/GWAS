a7c91e2d8d4a44823e2d24c2758948c80ab66490
David Kulp <dkulp@dizz.org>
2015-05-04 18:09:33 -0400

    initial commit

---
+++ R/code0.r
@@ -0,0 +1,7 @@
+#Code Snippet 0: run this once interactively to download and install BioConductor and the snpStats package
+
+source("http://bioconductor.org/biocLite.R")
+biocLite("snpStats")
+biocLite("SNPRelate")
+biocLite("chopsticks")
+install.packages('plyr')
