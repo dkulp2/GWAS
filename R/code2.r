051903a4da9a3fc75d35930e709ba37dc4ab7da1
Eric Reed <ericreed@Erics-MacBook-Pro.local>
2015-05-11 15:48:32 -0400

    Removed HWE filtering from code2.r.  Other changes were negligible.

--- R/code2.r
+++ R/code2.r
@@ -10,11 +10,10 @@ load(genotype.subset.fname)
 # Setting thresholds
 call <- 0.9
 minor <- 0.01
-hardy <- 10^-6
 
 library(snpStats)
 
-# Create SNP summary statistics (MAF, Call rate, Hardy-Weinberg, etc.)
+# Create SNP summary statistics (MAF, call rate, etc.)
 snpsum.col <- col.summary(genotype)
 
 # Filter on MAF and call rate
@@ -25,22 +24,9 @@ cat(ncol(genotype)-sum(use),"SNPs will be removed due to low MAF or call rate.\n
 
 # Subset genotype and SNP summary data for SNPs that pass call rate and MAF criteria
 genotype <- genotype[,use]
-snpsum.col<-snpsum.col[use,]
+snpsum.col <- snpsum.col[use,]
 
-# Filtering on HWE in CAD controls only
-CADcontrols <- clinical[ clinical$CAD==0, 'FamID' ]
-snpsum.colCont <- col.summary( genotype[CADcontrols,] )
-HWEuse <- with(snpsum.colCont, !is.na(z.HWE) & ( abs(z.HWE) < abs( qnorm(hardy/2) ) ) )
-rm(snpsum.colCont)
-
-HWEuse[is.na(HWEuse)] <- FALSE          # Remove NA's as well
-cat(ncol(genotype)-sum(HWEuse),"SNPs will be removed due to high HWE.\n")  # 2208 SNPs removed
-
-# Subset genotype and SNP summary data for SNPs that pass HWE criteria
-genotype <- genotype[,HWEuse]
-snpsum.col <- snpsum.col[HWEuse,]
-
-print(dim(genotype))                           # 695185 SNPs remain
+print(dim(genotype))                           # 697393 SNPs remain
 
 ##################
 
