7e56a85b6c8db2504343a3318297dbf3c655c6da
David Kulp <dkulp@dizz.org>
2015-05-13 17:46:02 -0400

    new working.data.fname and removed bonferonni

--- R/globals.R
+++ R/globals.R
@@ -1,11 +1,12 @@
+# ---- globals ----
+# Customize as needed for file locations
+
 # Modify data.dir to indicate the location of the GWAStutorial files
 # Intermediate data files will also be stored in this same location unless you set out.dir
-
-# customize as needed
-data.dir <- '/Users/ericreed/Desktop/FoulkesLab/SIMFiles'
+data.dir <- '/Volumes/genome/Research/GWAS'
 out.dir <- data.dir                     # may want to write to a separate dir to avoid clutter
 
-genotype.subset.fname <- sprintf("%s/subsetted_genotype.RData",out.dir)
+working.data.fname <- sprintf("%s/subsetted_genotype.RData",out.dir)
 
 gwas.fn = lapply(c(bed='bed',bim='bim',fam='fam',gds='gds'), function(n) sprintf("%s/GWAStutorial.%s", data.dir, n))
 gwaa.fname <- sprintf("%s/GWAStutorialout.txt", out.dir)
@@ -16,7 +17,3 @@ CETP.fname <- sprintf("%s/CETP_GWASout.csv", out.dir)
 
 hapmap.url <- "ftp://ftp.ncbi.nlm.nih.gov/hapmap/genotypes/2009-01_phaseIII/hapmap_format/polymorphic/genotypes_chr%s_CEU_phase3.2_nr.b36_fwd.txt.gz"
 
-
-bonferroni.alpha <- 0.05
-bonferroni.adjustment <- 1000000
-bonferroni.thresh <- -log10(bonferroni.alpha / bonferroni.adjustment)
