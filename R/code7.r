758eea6c27cf975cbd6878db956be8a341db601a
ericreed <erreed@schoolph.umass.edu>
2015-05-15 15:51:10 -0400

    Only superficial changes to code notes

--- R/code7.r
+++ R/code7.r
@@ -10,15 +10,15 @@ load(working.data.fname)
 library(snpStats)
 library(plyr)
 
-# Carry out association testing for imputed SNPs using single.snp.tests()
+# Carry out association testing for imputed SNPs using snp.rhs.tests()
 rownames(phenoSub) <- phenoSub$id
 
 imp <- snp.rhs.tests(phenotype ~ sex + age + pc1 + pc2 + pc3 + pc4 + pc5 + pc6 + pc7 + pc8 + pc9 + pc10, family = "Gaussian", data = phenoSub, snp.data = target, rules = rules)
 
 # Obtain p values for imputed SNPs
 pVals <- p.value(imp)
-results <- data.frame(SNP=imp@snp.names, p.value=pVals)
-results<- results[!is.na(results$p.value),]
+results <- data.frame(SNP = imp@snp.names, p.value = pVals)
+results <- results[!is.na(results$p.value),]
 
 
 #Write a file containing the results
@@ -49,5 +49,5 @@ impCETPgeno <- imputed[, colnames(imputed) %in% impCETP$SNP ]
 ##################
 
 save(genotype, genoBim, clinical, pcs, imputed, target, rules,
-     phenoSub, genes, impCETP, impCETPgeno, imputeOut, file=working.data.fname)
+     phenoSub, genes, impCETP, impCETPgeno, imputeOut, file = working.data.fname)
 
