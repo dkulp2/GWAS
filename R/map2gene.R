b0441a3a053354f2c62265e90ca331d19e1bb931
David Kulp <dkulp@dizz.org>
2015-05-20 10:02:35 -0400

    trivial

--- R/map2gene.R
+++ R/map2gene.R
@@ -1,10 +1,11 @@
-map2gene <- function(gene, coords, SNPs, extend.boundary = 5000){
+map2gene <- function(gene, coords, SNPs, extend.boundary = 5000) {
   coordsSub <- coords[coords$gene == gene,] #Subset coordinate file for spcified gene
   
   coordsSub$start <- coordsSub$start - extend.boundary # Extend gene boundaries
   coordsSub$stop <- coordsSub$stop + extend.boundary
   
-  SNPsub<-SNPs[SNPs$position >= coordsSub$start & SNPs$position <= coordsSub$stop &
+  SNPsub <- SNPs[SNPs$position >= coordsSub$start & SNPs$position <= coordsSub$stop &
                  SNPs$chr == coordsSub$chr,] #Subset for SNPs in gene
   
-  return(data.frame(SNPsub, gene = gene, stringsAsFactors = FALSE))}
+  return(data.frame(SNPsub, gene = gene, stringsAsFactors = FALSE))
+}
