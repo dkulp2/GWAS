85745f4e55f3bc0a81fc35b5f7b1ede21b52934c
ericreed <erreed@schoolph.umass.edu>
2015-05-29 13:57:35 -0400

    Change the results object to do stringsAsFactors = FALSE. This also fixes it.

--- R/code7.r
+++ R/code7.r
@@ -18,7 +18,7 @@ imp <- snp.rhs.tests(phenotype ~ sex + age + pc1 + pc2 + pc3 + pc4 + pc5 + pc6 +
                      family = "Gaussian", data = phenoSub, snp.data = target, rules = rules)
 
 # Obtain p values for imputed SNPs by calling methods on the returned GlmTests object.
-results <- data.frame(SNP = imp@snp.names, p.value = p.value(imp))
+results <- data.frame(SNP = imp@snp.names, p.value = p.value(imp), stringsAsFactors = FALSE)
 results <- results[!is.na(results$p.value),]
 
 #Write a file containing the results
@@ -43,7 +43,6 @@ genes <- read.csv(protein.coding.coords.fname, stringsAsFactors = FALSE)
 
 # Subset for CETP SNPs
 impCETP <- map2gene("CETP", coords = genes, SNPs = imputeOut)
-impCETP$SNP <- as.character(impCETP$SNP)
 
 # Filter only the imputed CETP SNP genotypes 
 impCETPgeno <- imputed[, impCETP$SNP ]
