ca0c6ff99a159609684ddbbdabe7968dc370330c
ericreed <erreed@schoolph.umass.edu>
2015-06-23 12:02:00 -0400

    Added code to download the data from the internet.

--- R/code0.r
+++ R/code0.r
@@ -1,4 +1,7 @@
 # ---- code0 ----
+source("globals.R")
+
+# ---- code0-a ----
 # Run this once interactively to download and install BioConductor packages and other packages.
 
 source("http://bioconductor.org/biocLite.R")
@@ -10,4 +13,11 @@ install.packages(c('plyr', 'GenABEL', 'LDheatmap','doParallel', 'ggplot2', 'coin
 
 library(devtools)
 install_url("http://cran.r-project.org/src/contrib/Archive/postgwas/postgwas_1.11.tar.gz")
+
+
+# ---- code0-b ----
+# Download and unzip data needed for this tutorial
+
+download.file(urlSupport, zipSupport.fn, method = "curl")
+unzip(zipSupport.fn, exdir = data.dir)
 
